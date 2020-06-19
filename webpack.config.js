76aafe6652daa0d855138b8e4d74833e448c4a3a
한단비 <onedanbee@gmail.com>
2020-06-19 18:58:40 +0900

    Fix sass

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const BrowserSyncPlugin = require("browser-sync-webpack-plugin");
 const path = require("path");
 
 const basePath = __dirname;
@@ -49,6 +50,7 @@ module.exports = {
       {
         test: /\.(sa|sc|c)ss$/,
         use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+        exclude: /node_modules/,
       },
       {
         test: /\.jsx?$/,
@@ -66,6 +68,12 @@ module.exports = {
     new MiniCssExtractPlugin({
       filename: "[name].css",
       chunkFilename: "[id].css",
+    }),
+    new BrowserSyncPlugin({
+      host: "localhost", //localhost로 사용
+      port: 8080, //포트 3000을 사용  (이미 사용중이면 1씩 증가된 포트로 사용)
+      files: ["./dist/*.html"], //해당 경로 내 html 파일이 자동으로 동기화 (이 부분이 없으면 html파일 변경사항은 자동 동기화 안됨)
+      // server: { baseDir: ["dist"] }, // server의 Base 디렉토리를 dist로 지정
     }),
   ],
 };
